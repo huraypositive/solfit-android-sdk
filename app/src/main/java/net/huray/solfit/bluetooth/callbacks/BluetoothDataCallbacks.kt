ea36d7947f99bb4c0eccd1d1777ebe3bbae19037
ParkTaeKyu <taekyu2015@gmail.com>
2021-11-25 11:33:57 +0900

    [REFACTOR] 주석 수정

--- app/src/main/java/net/huray/solfit/bluetooth/callbacks/BluetoothDataCallbacks.kt
+++ app/src/main/java/net/huray/solfit/bluetooth/callbacks/BluetoothDataCallbacks.kt
@@ -7,22 +7,17 @@ import net.huray.solfit.bluetooth.data.enums.WeightState
 
 interface BluetoothDataCallbacks {
 
-    // STATE: 측정 시작(WeightState_START = 0)
-    //        대기 상태(WeightState_WAITING = 1)
-    //        측정 성공(WeightState_SUCCESS = 2)
+    /**
+     * STATE: 측정 시작(WeightState_START = 0)
+     *        대기 상태(WeightState_WAITING = 1)
+     *        측정 성공(WeightState_SUCCESS = 2)
+     */
     fun onGetWeight(state: WeightState, weightData: Double?)
 
-    // STATE: 측정 시작(BodyCompositionState_START = 20)
-    //        측정 실패(BodyCompositionState_FAIL = 21)
-    //        측정 성공(BodyCompositionState_SUCCESS =22)
+    /**
+     * STATE: 측정 시작(BodyCompositionState_START = 20)
+     *        측정 실패(BodyCompositionState_FAIL = 21)
+     *       측정 성공(BodyCompositionState_SUCCESS =22)
+     */
     fun onGetBodyComposition(state: BodyCompositionState, bodyFatData: BodyFatData?, moreFatData: MoreFatData?)
 }
-
-/**
- *
- * todo
-enum
-fatdata, musclemass
-
-유저정보, 디바이스정보 저
-**/
--- app/src/main/java/net/huray/solfit/bluetooth/callbacks/BluetoothScanCallbacks.kt
+++ app/src/main/java/net/huray/solfit/bluetooth/callbacks/BluetoothScanCallbacks.kt
@@ -4,8 +4,10 @@ import aicare.net.cn.iweightlibrary.entity.BroadData
 import net.huray.solfit.bluetooth.data.enums.ScanState
 
 interface BluetoothScanCallbacks {
-    // STATE : ScanState_FAIL(실패)
-    //         ScanState_SCANNING(성공)
-    //         ScanState_STOPPED(종료)
+    /**
+     *  STATE : ScanState_FAIL(실패)
+     *          ScanState_SCANNING(성공)
+     *          ScanState_STOPPED(종료)
+     */
     fun onScan(state: ScanState, errorMsg: String?, deviceList: List<BroadData>?)
 }
